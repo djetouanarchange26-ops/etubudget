9122aebbfe8c081402f143141b0ec1e52948474d
Djetouan Archange <archange.djetouan@student-cs.fr>
2026-04-12 18:43:38 +0200

    fonction: résumé mensuel et dernières transactions écrites

--- database/queries.py
+++ database/queries.py
@@ -17,3 +17,32 @@ def get_user_by_username(username):
     if user is None: #si aucun utilisateur n'est trouvé, retourne None
         return None
     return dict(user) #si un utilisateur est trouvé, retourne un dictionnaire contenant les informations de l'utilisateur
+
+def get_monthly_summary(user_id, month):
+    conn = get_connection() #obtenir la connexion à la base de données
+    nombre_transactions = conn.execute("""
+        SELECT COUNT(*) AS total_transactions from transactions WHERE strftime('%Y-%m', date) = ? AND user_id = ?
+    """, (month, user_id)).fetchone()[0] #exécuter une requête pour récupérer le résumé mensuel des transactions de l'utilisateur pour le mois spécifié
+
+    total_revenu = conn.execute("""
+        SELECT COALESCE(SUM(amount), 0) from transactions WHERE type = "revenu" AND strftime('%Y-%m', date) = ? AND user_id = ? AND amount > 0
+    """, (month, user_id)).fetchone()[0] #exécuter une requête pour récupérer le total des revenus de l'utilisateur pour le mois spécifié
+
+    total_depense = conn.execute("""
+        SELECT COALESCE(SUM(amount), 0) from transactions WHERE type = "depense" AND strftime('%Y-%m', date) = ? AND user_id = ? AND amount > 0
+    """, (month, user_id)).fetchone()[0] #exécuter une requête pour récupérer le total des dépenses de l'utilisateur pour le mois spécifié
+
+    return {
+    "nb_transactions": nombre_transactions,
+    "total_revenus":   total_revenu,
+    "total_depenses":  total_depense,
+    "solde":           (total_revenu or 0) - (total_depense or 0)}
+
+def get_last_transactions(user_id, limit=5):
+    conn = get_connection() #obtenir la connexion à la base de données
+    transactions = conn.execute("""
+        SELECT * FROM transactions
+        LEFT JOIN categories ON transactions.category_id = categories.id
+        WHERE user_id = ? ORDER BY date DESC LIMIT ?
+    """, (user_id, limit)).fetchall() #exécuter une requête pour récupérer les dernières transactions de l'utilisateur, triées par date décroissante et limitées au nombre spécifié
+    return [dict(r) for r in transactions] #retourner une liste de dictionnaires contenant les informations des transactions récupérées
